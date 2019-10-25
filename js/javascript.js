9f05bd56c72f4a4516cb999b325fac0e83ad24ce
Josh West <josh.west@centre.edu>
2019-10-24 20:57:21 -0400

    cool cursor

--- js/javascript.js
+++ js/javascript.js
@@ -5,7 +5,9 @@ var setBanner = function(message)
 }
 
 var filmPromise=
-    d3.json("https://swapi.co/api/films/")
+d3.json("https://swapi.co/api/films/")
+//d3.json("https://ghibliapi.herokuapp.com/films")
+
 filmPromise.then(
 function(films)
 {
@@ -27,10 +29,34 @@ var getFilmList = function(films)
     .enter()
     .append("div")
     .text(function(film){return film.title})
-    .on("click", function(d){getInfoList(d)}) 
+    //.on("click", function(d){getInfoList(d)}) 
 }
-/*
-var getInfoList = function(films)
+
+/*var getInfoList = function(films)
 {
+    d3.select("#infoList")
     
+}*/
+
+/*var changeCSS = function(allegiance) // do this when light or dark is clicked
+{
+    d3.select(link)
+    .on("click", function(
+    {
+        if (click == #light)
+        {
+            
+        }
+    }))
+    .attr("href", function(alegiance)
+         {
+            if (allegiance = light) // make sure to attach a new allegiance attr when light clicked
+            {
+                return "light.css"
+            }
+            else if (allegiance = dark) // make sure to attach a new allegiance attr when dark clicked
+            {
+                return "dark.css"
+            }
+    })
 }*/
