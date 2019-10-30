a41e5fe4c69afbd5cddd35b7bcd7580511c3c058
Josh West <josh.west@centre.edu>
2019-10-29 20:04:10 -0400

    made progress

--- js/javascript.js
+++ js/javascript.js
@@ -4,17 +4,20 @@ var setBanner = function(message)
     .text(message)
 }
 var consoleFilms = [];
-var filmPromise=
+var filmPromise =
 d3.json("https://swapi.co/api/films/")
 //d3.json("https://ghibliapi.herokuapp.com/films") //Works with this API bc array, but not with Star Wars
 
 filmPromise.then(
 function(films)
 {   
-    consoleFilms = films; // DO NOT USE FOR CODING; Works for console
+    consoleFilms = films.results; // DO NOT USE FOR CODING; Works for console
+    console.log(consoleFilms);
     setBanner("STAR WARS API");
     getFilmList(films.results);// Passes the actual array to the rest of the "films" in the program
     console.log("films", films)
+    var urls = films.results[0].opening_crawl;
+    console.log(urls);
 },
 function(err)
 {
@@ -30,15 +33,67 @@ var getFilmList = function(films)
     .enter()
     .append("div")
     .text(function(film){return film.title})
-    //.on("click", function(d){getInfoList(d)}) 
+    .attr("class", function(d) {return d.episode_id}) //gives each film a unique class
+    //.on("click", function(){alert("WORKS")}) 
+    .on("click", function(d){
+       console.log("clicked");
+       //getInfoList(films);
+       if(d.episode_id == "4"){
+            console.log("first movie in the list")
+            getInfoList(films[0]);
+           console.log(films[0])
+       }
+        else if(d.episode_id == "2"){
+            console.log("second movie in the list")
+            getInfoList(films[1])}
+       else if(d.episode_id == "1"){
+           console.log("third movie in the list")}
+       else if(d.episode_id == "3"){
+           console.log("fourth movie in the list")}
+       else if(d.episode_id == "6"){
+           console.log("fifth movie in the list")}
+       else if(d.episode_id == "5"){
+           console.log("sixth movie in the list")}
+       else if(d.episode_id == "7"){
+           console.log("seventh movie in the list")}
+   })
 }
 
-/*var getInfoList = function(films)
+var getInfoList = function(films)
+{
+    console.log("inside infolist")
+    d3.select("#infoList")
+    .selectAll("div")
+    .data(films)
+    .enter()
+    .append("span")
+    .text(function(film){
+    return film.opening_crawl       
+                   })
+}
+var clearInfo = function()
 {
     d3.select("#infoList")
-    
-}*/
+    .selectAll("div")
+    .remove()
+}
+/*
+if(film.episode_id == "4"){
+           return film.opening_crawl}
+        else if(film.episode_id == "2"){
+           return films[1].opening_crawl}
+       else if(film.episode_id == "1"){
+           return films[2].opening_crawl}
+       else if(film.episode_id == "3"){
+           return films[3].opening_crawl}
+       else if(film.episode_id == "6"){
+           return films[4].opening_crawl}
+       else if(film.episode_id == "5"){
+           return films[5].opening_crawl}
+       else if(film.episode_id == "7"){
+          return films[6].opening_crawl}
 
+*/
 /*var changeCSS = function(allegiance) // do this when light or dark is clicked
 {
     d3.select(link)
